1ad16b0dbeb54ce8f58713998214bc8f2dbf33ec
hanjoeAi <hanjoe278@gmail.com>
2025-08-13 22:55:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     // ########## PENGATURAN ##########
-    const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxjYPUmS-hBAkOhq5KRTGJKrYBNeiPndj183HESZctjuA7RN0Yd-fAsOqinWUIc0ECM7g/exec"; // GANTI DENGAN URL WEB APP ANDA
+    const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz5mnZ54X-J_VJZnXg-CUix4MopWBmJwf_3XOWuUeYOMXtXw8abFFEXrSIhdLh0R6qucg/exec"; // GANTI DENGAN URL WEB APP ANDA
     const PELATIH_PENDAMPING = [
         { nama: "Hendrikus Y. Mori", peran: "Pelatih" },
         { nama: "Benediktus Budi", peran: "Pendamping 1" },
